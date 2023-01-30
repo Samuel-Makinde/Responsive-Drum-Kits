3ec0de488d925e8c7465f1a7934816016d2026bd
Samuel Makinde <makindesamuel1999@gmail.com>
2023-01-30 15:28:16 +0100

    new update

---
+++ code.js
@@ -0,0 +1,21 @@
+const kits = ["crash", "Hit-Hat", "kick", "snare", "tom"];
+const containerEl = document.querySelector(".container");
+
+kits.forEach((kit=>{
+    const btnEl = document.createElement("button");
+    btnEl.classList.add("btn");
+    btnEl.innerHTML = kit;
+    btnEl.style.backgroundImage = "url(images/" + kit + ".jpg";
+    containerEl.appendChild(btnEl);
+    const audioEl = document.createElement("audio");
+    audioEl.src = "sounds/" + kit + ".mp3";
+    containerEl.appendChild(audioEl);
+    btnEl.addEventListener("click", ()=>{
+        audioEl.play();
+    })
+    window.addEventListener("keydown", (event) => {
+        if(event.key === kit.slice(0, 1)){
+        audioEl.play();
+        }
+    })
+}))
